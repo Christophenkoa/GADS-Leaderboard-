2b60a4bcd7cb44f815bcf21f3c5b8bf521bd8e74
Nkoa Christophe <nkoachristophe@gmail.com>
2020-09-01 13:15:56 +0100

    main design done

--- app/src/main/java/com/example/gadsleaderboard/MainActivity.java
+++ app/src/main/java/com/example/gadsleaderboard/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.gadsleaderboard;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //hide action bar
+        getSupportActionBar().hide();
+
+        // wait 8s before going to the next activity
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //Write whatever to want to do after delay specified (1 sec)
+                Intent intent = new Intent(MainActivity.this, ListActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 4000);
     }
+
 }
