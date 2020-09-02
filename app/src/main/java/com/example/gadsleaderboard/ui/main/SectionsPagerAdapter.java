6eafab996a1f38831d29d856d369348bbad9feda
Nkoa Christophe <nkoachristophe@gmail.com>
2020-09-02 11:52:06 +0100

    designing recycle views

--- app/src/main/java/com/example/gadsleaderboard/ui/main/SectionsPagerAdapter.java
+++ app/src/main/java/com/example/gadsleaderboard/ui/main/SectionsPagerAdapter.java
@@ -8,7 +8,9 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 
+import com.example.gadsleaderboard.LearningFragment;
 import com.example.gadsleaderboard.R;
+import com.example.gadsleaderboard.SkillFragment;
 
 /**
  * A [FragmentPagerAdapter] that returns a fragment corresponding to
@@ -27,9 +29,17 @@ public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
     @Override
     public Fragment getItem(int position) {
-        // getItem is called to instantiate the fragment for the given page.
-        // Return a PlaceholderFragment (defined as a static inner class below).
-        return PlaceholderFragment.newInstance(position + 1);
+        Fragment fragment = null;
+        switch (position) {
+            case 0:
+                fragment = new LearningFragment();
+                break;
+
+            case 1:
+                fragment = new SkillFragment();
+                break;
+        }
+        return fragment;
     }
 
     @Nullable
